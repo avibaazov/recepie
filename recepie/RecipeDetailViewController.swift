3019af721fbf377e11cd6240ef2d9f3913fdefb4
avi baazov <avibaazov2@gmail.com>
2024-08-13 01:33:41 +0300

    recipe

--- recepie/RecipeDetailViewController.swift
+++ recepie/RecipeDetailViewController.swift
@@ -18,7 +18,7 @@ class RecipeDetailViewController: UIViewController,UITableViewDataSource, UITabl
             
             if let recipe = recipe {
                 titleLabel.text = recipe.title
-                instructionsButton.setTitle("Instructions", for: .normal)  // Assuming you have a button to show instructions
+//                instructionsButton.setTitle("Instructions", for: .normal)  // Assuming you have a button to show instructions
                 cookTimeLabel.text = "\(recipe.cookTime) mins"
                 servingsLabel.text = " \(recipe.servings) serve"
                 if let imageUrl = recipe.imageUrl, let url = URL(string: imageUrl) {
@@ -38,6 +38,7 @@ class RecipeDetailViewController: UIViewController,UITableViewDataSource, UITabl
         private func configureTableView() {
             ingredientsTableView.dataSource = self
             ingredientsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "IngredientCell")
+            
         }
 
         // MARK: - UITableViewDataSource Methods
@@ -46,9 +47,13 @@ class RecipeDetailViewController: UIViewController,UITableViewDataSource, UITabl
             return recipe?.ingredients.count ?? 0
         }
 
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientCell", for: indexPath)
-            cell.textLabel?.text = recipe?.ingredients[indexPath.row]
-            return cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientCell", for: indexPath)
+        if let ingredient = recipe?.ingredients[indexPath.row] {
+            cell.textLabel?.font = UIFont.systemFont(ofSize: 16)
+            cell.textLabel?.textColor = UIColor.darkGray
+            cell.textLabel?.text = "• \(ingredient)"
         }
+        return cell
+    }
     }
